99c62eef726612ccf011f8b8c5a0cacc70d5591a
jaredcnance <jaredcnance@gmail.com>
2018-04-14 10:52:59 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-2365757ffa1098796338.js","dc235abbf20a78bf70ee41121212b176"],["/app-a5e665fcdbad95c1eeb3.js","95b404f0b7d851b925ca6594a4107e9a"],["/app-dca761cfa7c9545bd6ef.js","6e564818b30e50507e759c34c7be1009"],["/app-fc0aa8571a96e457f8e7.js","0929ff928d8618ccbf318c7f3cfa22b1"],["/commons-052854b14b2d9cebb713.js","efaac7a7e9870808e48f2f82aae8694a"],["/index.html","1828392cdc49b7f7331f70a15f42e47f"],["/offline-plugin-app-shell-fallback/index.html","716dfb94b084927ec3bcb8e4342cc13b"]];
+var precacheConfig = [["/app-2365757ffa1098796338.js","dc235abbf20a78bf70ee41121212b176"],["/app-a5e665fcdbad95c1eeb3.js","95b404f0b7d851b925ca6594a4107e9a"],["/app-dca761cfa7c9545bd6ef.js","6e564818b30e50507e759c34c7be1009"],["/app-fc0aa8571a96e457f8e7.js","0929ff928d8618ccbf318c7f3cfa22b1"],["/commons-052854b14b2d9cebb713.js","efaac7a7e9870808e48f2f82aae8694a"],["/index.html","faf46bd9000096a019b3c6dc6abb92f6"],["/offline-plugin-app-shell-fallback/index.html","3932bb43c0006d01f9efd57dfffd94a3"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
